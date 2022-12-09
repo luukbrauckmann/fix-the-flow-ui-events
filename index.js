2a52cef80922fd14f2c39d3a71cbd9a82952fb52
Luuk <luukbrauckmann@gmail.com>
2022-12-09 14:35:30 +0100

    Events worden afgehandeld Je kunt kiezen uit events

---
+++ index.js
@@ -0,0 +1,16 @@
+colorButton = (event) => {
+	const { target } = event
+	if (target.matches('a')) target.classList.add('purple')
+	setTimeout(() =>  target.classList.remove('purple'), 2000);
+}
+
+var activeEvent = 'click'
+document.addEventListener(activeEvent, colorButton)
+
+const select = document.getElementById('events')
+select.addEventListener('change', (event) => {
+	document.removeEventListener(activeEvent, colorButton)
+	activeEvent = event.target.value
+	document.addEventListener(activeEvent, colorButton)
+})
+
