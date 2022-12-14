6aedca77b388094156f3cf1641b9dad6767b1bf1
Luuk <luukbrauckmann@gmail.com>
2022-12-14 14:17:26 +0100

    Commit

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-let missCounter = +localStorage.getItem('misscount') || 0
-const missCounterElement = document.getElementById('miss-count')
-missCounterElement.innerText = missCounter
+var longpressed = false
+var timeout
 
-const button = document.getElementById('impossible-button')
-button.addEventListener('mouseover', (event) => {
-	const { target } = event
-	// console.log(window.innerHeight, window.innerWidth);
 
-	const missMsg = document.getElementById('miss-msg')
-	missMsg.style.transform = target.style.transform
-	missMsg.style.opacity = 1
-	setTimeout(() => missMsg.style.opacity = 0, 600);
+buttonIsLongPressed = () => {
+	console.log('Yes');
+}
 
-	const directionX = Math.random() < 0.5 ? '+' : '-'
-	const directionY = Math.random() < 0.5 ? '+' : '-'
-	const x = target.style.transform.substring(target.style.transform.indexOf('X(') + 2, target.style.transform.indexOf('%)'))
-	const y = target.style.transform.substring(target.style.transform.indexOf('Y(') + 2, target.style.transform.lastIndexOf('%)'))
+const button = document.getElementById('longpress-button')
+button.addEventListener('mousedown', () => {
+	timeout = setTimeout(buttonIsLongPressed, 2000)
+})
 
-	const newX = x ? eval(`${x} ${directionX} 100`) : eval(`0 ${directionX} 100`)
-	const newY = y ? eval(`${y} ${directionY} 100`) : eval(`0 ${directionY} 100`)
-	target.style.transform = `translateX(${newX}%) translateY(${newY}%)`
-
-	// const positionLeft = target.getBoundingClientRect().left
-	// const positionRight = target.getBoundingClientRect().top
-	// console.log(positionLeft < 0 && positionLeft > window.innerWidth);
-	// console.log(positionRight < 0 && positionRight > window.innerHeight);
-	// const buttonOutOfView = positionLeft < 0 && positionLeft > window.innerWidth || positionRight < 0 && positionRight > window.innerHeight
-	// if (buttonOutOfView) target.style.transform = `translateX(-50%) translateY(-50%)`
-
-	missCounterElement.innerText = ++missCounter
-	localStorage.setItem('misscount', missCounter)
+button.addEventListener('mouseup', () => {
+	timeout.clear
 })
