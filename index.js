5b511828532c7eaed5ec8fc3fdf1d54ee02c41f8
Luuk <luukbrauckmann@gmail.com>
2022-12-14 12:56:27 +0100

    The impossible button

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-colorButton = (event) => {
+let missCounter = +localStorage.getItem('misscount') || 0
+const missCounterElement = document.getElementById('miss-count')
+missCounterElement.innerText = missCounter
+
+const button = document.getElementById('impossible-button')
+button.addEventListener('mouseover', (event) => {
 	const { target } = event
-	if (target.matches('a')) target.classList.add('purple')
-	setTimeout(() =>  target.classList.remove('purple'), 2000);
-}
+	const directionX = Math.random() < 0.5 ? '+' : '-'
+	const directionY = Math.random() < 0.5 ? '+' : '-'
+	const x = target.style.transform.substring(target.style.transform.indexOf('X(') + 2, target.style.transform.indexOf('%)'))
+	const y = target.style.transform.substring(target.style.transform.indexOf('Y(') + 2, target.style.transform.lastIndexOf('%)'))
 
-var activeEvent = 'click'
-document.addEventListener(activeEvent, colorButton)
+	const newX = x ? eval(`${x} ${directionX} 100`) : eval(`0 ${directionX} 100`)
+	const newY = y ? eval(`${y} ${directionY} 100`) : eval(`0 ${directionY} 100`)
+	target.style.transform = `translateX(${newX}%) translateY(${newY}%)`
 
-const select = document.getElementById('events')
-select.addEventListener('change', (event) => {
-	document.removeEventListener(activeEvent, colorButton)
-	activeEvent = event.target.value
-	document.addEventListener(activeEvent, colorButton)
+	missCounterElement.innerText = ++missCounter
+	localStorage.setItem('misscount', missCounter)
 })
-
