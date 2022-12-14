a56321bf6ac78dda3511b694566983b8e654c7cc
Luuk <luukbrauckmann@gmail.com>
2022-12-14 14:04:16 +0100

    Deels uit het scherm reset

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ missCounterElement.innerText = missCounter
 const button = document.getElementById('impossible-button')
 button.addEventListener('mouseover', (event) => {
 	const { target } = event
+	// console.log(window.innerHeight, window.innerWidth);
+
+	const missMsg = document.getElementById('miss-msg')
+	missMsg.style.transform = target.style.transform
+	missMsg.style.opacity = 1
+	setTimeout(() => missMsg.style.opacity = 0, 600);
+
 	const directionX = Math.random() < 0.5 ? '+' : '-'
 	const directionY = Math.random() < 0.5 ? '+' : '-'
 	const x = target.style.transform.substring(target.style.transform.indexOf('X(') + 2, target.style.transform.indexOf('%)'))
@@ -13,6 +20,13 @@ button.addEventListener('mouseover', (event) => {
 	const newX = x ? eval(`${x} ${directionX} 100`) : eval(`0 ${directionX} 100`)
 	const newY = y ? eval(`${y} ${directionY} 100`) : eval(`0 ${directionY} 100`)
 	target.style.transform = `translateX(${newX}%) translateY(${newY}%)`
+
+	// const positionLeft = target.getBoundingClientRect().left
+	// const positionRight = target.getBoundingClientRect().top
+	// console.log(positionLeft < 0 && positionLeft > window.innerWidth);
+	// console.log(positionRight < 0 && positionRight > window.innerHeight);
+	// const buttonOutOfView = positionLeft < 0 && positionLeft > window.innerWidth || positionRight < 0 && positionRight > window.innerHeight
+	// if (buttonOutOfView) target.style.transform = `translateX(-50%) translateY(-50%)`
 
 	missCounterElement.innerText = ++missCounter
 	localStorage.setItem('misscount', missCounter)
